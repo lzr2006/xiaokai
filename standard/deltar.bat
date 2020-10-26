9db5da6e1677c5c93889c5d585ccf9e7354cba71
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-10-27 06:22:41 +0800

    Create deltar.bat

--- standard/deltar.bat
+++ standard/deltar.bat
@@ -1,1 +1,3 @@
 del C:\users\administrator\desktop\xiaokai\standard\*.tar
+start C:\users\administrator\desktop\xiaokai\standard\Success.vbs
+start C:\users\administrator\desktop\xiaokai\tools\files.bat
