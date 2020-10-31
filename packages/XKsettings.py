b4158127d920e24fd02fe562f6170ebaac96573a
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-10-31 11:31:04 +0800

    基本修复完成 10.31

--- packages/XKsettings.py
+++ packages/XKsettings.py
@@ -2,6 +2,7 @@
 #小恺简易配置程序 /玩科光电/玩具世界科技部.
 from time import time,sleep
 from os import system
+import XKcommand
 print("Toysworld (©2016-2020) Studio. all rights reserved. Unix timestamp:" + str(int(time())) + ".")
 i = 0
 class do():
@@ -34,16 +35,21 @@ class do():
     	  sleep(3)
     	  print("Success!\n恭喜,您可以正常的使用小恺了.")
     	  sleep(3)
-    	
-while i == 0:
-    user = input("The Xiaokai Standard Library>")
-    if user == "normal":
-        print("自动一般模式处理开始...") 
-        do.normal()
-        break
-    elif user == "developer":
-    	print('开发者处理模式开始...')
-    	do.developer()
-    	break
-    else:
-        print(f"'{user}'并不是可行的模式,也不是可执行的命令.")
+
+    def todo():
+        i = 0
+        
+        while i == 0:
+            user = input("The Xiaokai Standard Library>")
+            if user == "normal":
+                print("自动一般模式处理开始...") 
+                do.normal()
+                XKcommand.Cmd_easy.kill('pythonw.exe')
+                i = 1
+            elif user == "developer":
+                print('开发者处理模式开始...')
+                do.developer()
+                XKcommand.Cmd_easy.kill('pythonw.exe')
+                i = 1
+            else:
+                print(f"'{user}'并不是可行的模式,也不是可执行的命令.")
