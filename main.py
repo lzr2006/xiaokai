15d933c1958985c2fefbd67b7def0ea4200bb497
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-10-31 11:28:47 +0800

    完善配置，修复bug

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ sys.path.append('C:\\users\\administrator\\desktop\\XiaoKai\\packages')
 #引入其他轮子
 import XKrandom
 import XKtranslation
+import XKsettings
 #定义列表区
 hello=['hi','hello','你好','你好啊','你好哇','您好','您好啊','嗨','嘿']
 
@@ -25,6 +26,7 @@ def restart():
     global i
     i = 0
 
+
 #程序主体-----------------------------------------程序主体#
 while True:
     restart()    
@@ -36,17 +38,7 @@ while True:
             webbrowser.open("https://www.sogou.com/sie?hdq=[AQxRG-0000&query="+str(user_import)+"&ie=utf8")
             print(f'为您搜索{user_import}')
             break
-        if user_import == "cmd":                   #cmd功能
-            print(" 温馨提示:输入'start'打开cmd窗口,输入cmd -s[command]可以运行指定命令行,-s与[command]之间不留空格")
-            os.system("cmd.exe")
-            break
-        elif user_import.startswith("cmd -s"):      #运行指定命令
-             cmd_control=user_import.split("-s",1)
-             cmd_control_user = cmd_control[1]
-             cmd_control_user=str(cmd_control_user)#提取用户输入
-             os.system("cmd /C" + cmd_control_user)
-             break
-        elif user_import == '随机数内核':
+        elif user_import == '随机数内核': #随机数模块.
             while i == 0:
                 user_input = input('toysworld\\random-system>')
                 if user_input == '随机排序':
@@ -63,11 +55,6 @@ while True:
             print('仅仅是调用接口，如需大批量翻译请勿使用本程序！')
             XKtranslation.main()
             break
-
-            
-            
-
-
-
-
+        elif user_import == '配置':
+            XKsettings.do.todo()
 
