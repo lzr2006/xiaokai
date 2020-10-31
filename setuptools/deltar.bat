445f2e4f49b19e2001f410b8001a91d74726f931
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-10-31 11:31:48 +0800

    去掉了无用功能

--- setuptools/deltar.bat
+++ setuptools/deltar.bat
@@ -1,3 +1,1 @@
 del C:\users\administrator\desktop\xiaokai\standard\*.tar
-start C:\users\administrator\desktop\xiaokai\standard\Success.vbs
-start C:\users\administrator\desktop\xiaokai\tools\files.bat
